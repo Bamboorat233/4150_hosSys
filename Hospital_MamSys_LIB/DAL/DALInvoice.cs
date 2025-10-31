9bdba181e1e86582f0855b4bf41ed315f41216fb
FabianLi2002 <likerui@uwindsor.ca>
2025-10-31 11:36:59 -0400

    Structural Design

---
+++ Hospital_MamSys_LIB/DAL/DALInvoice.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital_MamSys_LIB.DAL
+{
+    internal class DALInvoice
+    {
+    }
+}
