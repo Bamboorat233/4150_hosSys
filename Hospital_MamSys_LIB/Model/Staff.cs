583206d792419ad9d2a19f2ee418fbbfb35156a8
FabianLi2002 <likerui@uwindsor.ca>
2025-11-08 20:20:28 -0500

    Revert "Add Staff model implementation"
    This reverts commit f2e57a935751445aa2b7f24d01f43ad4f999027b.

--- Hospital_MamSys_LIB/Model/Staff.cs
+++ Hospital_MamSys_LIB/Model/Staff.cs
@@ -4,14 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Hospital_ManSys_LIB.Model
+namespace Hospital_MamSys_LIB.Model
 {
     internal class Staff
     {
-        public int StaffID { get; set; }
-        public string Name { get; set; } = "";
-        public string Role { get; set; } = "";
-        public string? Contact { get; set; }
-        public int? DepartmentID { get; set; }
     }
 }
