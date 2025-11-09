7fef5fe397a00575823246d8ebc1ca3aa7317a55
FabianLi2002 <likerui@uwindsor.ca>
2025-11-08 20:44:56 -0500

    staff update

--- Hospital_MamSys_LIB/Model/Staff.cs
+++ Hospital_MamSys_LIB/Model/Staff.cs
@@ -4,9 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Hospital_MamSys_LIB.Model
+namespace Hospital_ManSys_LIB.Model
 {
     internal class Staff
     {
+        public int StaffID { get; set; }
+        public string Name { get; set; } = "";
+        public string Role { get; set; } = "";
+        public string? Contact { get; set; }
+        public int? DepartmentID { get; set; }
     }
 }
