081dfa57527513f147afe262706ce3a52e013e7f
ruiliang ma <maruiliang666@gmail.com>
2025-11-09 14:14:55 -0500

    Add CRUD methods

--- Hospital_MamSys_LIB/DAL/DALAppointment.cs
+++ Hospital_MamSys_LIB/DAL/DALAppointment.cs
@@ -8,10 +8,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Hospital_ManSys_LIB.DAL
+namespace Hospital_MamSys_LIB.DAL
 {
     internal class DALAppointment : DALBase
     {
+        // 1) CREATE
         public void AddAppointment(Appointment a)
         {
             using (SqlConnection conn = new SqlConnection(connStr))
@@ -25,6 +26,102 @@ namespace Hospital_ManSys_LIB.DAL
                 cmd.Parameters.AddWithValue("@time", a.AppointmentTime);
                 cmd.Parameters.AddWithValue("@s", a.Status);
 
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        // 2) READ - get one by ID
+        public Appointment GetAppointmentById(int id)
+        {
+            using (SqlConnection conn = new SqlConnection(connStr))
+            using (SqlCommand cmd = new SqlCommand(
+                "SELECT AppointmentID, PatientID, DoctorID, AppointmentDate, AppointmentTime, Status " +
+                "FROM Appointment WHERE AppointmentID = @id", conn))
+            {
+                cmd.Parameters.AddWithValue("@id", id);
+                conn.Open();
+
+                using (SqlDataReader rdr = cmd.ExecuteReader())
+                {
+                    if (rdr.Read())
+                    {
+                        return new Appointment
+                        {
+                            AppointmentID = rdr.GetInt32(0),
+                            PatientID = rdr.GetInt32(1),
+                            DoctorID = rdr.GetInt32(2),
+                            AppointmentDate = rdr.GetDateTime(3),
+                            AppointmentTime = (TimeSpan)rdr["AppointmentTime"],
+                            Status = rdr.GetString(5)
+                        };
+                    }
+                }
+            }
+
+            return null;    
+        }
+
+        // 3) READ - get all
+        public List<Appointment> GetAllAppointments()
+        {
+            List<Appointment> list = new List<Appointment>();
+
+            using (SqlConnection conn = new SqlConnection(connStr))
+            using (SqlCommand cmd = new SqlCommand(
+                "SELECT AppointmentID, PatientID, DoctorID, AppointmentDate, AppointmentTime, Status FROM Appointment",
+                conn))
+            {
+                conn.Open();
+                using (SqlDataReader rdr = cmd.ExecuteReader())
+                {
+                    while (rdr.Read())
+                    {
+                        list.Add(new Appointment
+                        {
+                            AppointmentID = rdr.GetInt32(0),
+                            PatientID = rdr.GetInt32(1),
+                            DoctorID = rdr.GetInt32(2),
+                            AppointmentDate = rdr.GetDateTime(3),
+                            AppointmentTime = (TimeSpan)rdr["AppointmentTime"],
+                            Status = rdr.GetString(5)
+                        });
+                    }
+                }
+            }
+
+            return list;
+        }
+
+        // 4) UPDATE
+        public void UpdateAppointment(Appointment a)
+        {
+            using (SqlConnection conn = new SqlConnection(connStr))
+            using (SqlCommand cmd = new SqlCommand(
+                "UPDATE Appointment SET " +
+                "PatientID = @p, DoctorID = @d, AppointmentDate = @date, AppointmentTime = @time, Status = @s " +
+                "WHERE AppointmentID = @id", conn))
+            {
+                cmd.Parameters.AddWithValue("@p", a.PatientID);
+                cmd.Parameters.AddWithValue("@d", a.DoctorID);
+                cmd.Parameters.AddWithValue("@date", a.AppointmentDate);
+                cmd.Parameters.AddWithValue("@time", a.AppointmentTime);
+                cmd.Parameters.AddWithValue("@s", a.Status);
+                cmd.Parameters.AddWithValue("@id", a.AppointmentID);
+
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        // 5) DELETE
+        public void DeleteAppointment(int id)
+        {
+            using (SqlConnection conn = new SqlConnection(connStr))
+            using (SqlCommand cmd = new SqlCommand(
+                "DELETE FROM Appointment WHERE AppointmentID = @id", conn))
+            {
+                cmd.Parameters.AddWithValue("@id", id);
                 conn.Open();
                 cmd.ExecuteNonQuery();
             }
--- Hospital_MamSys_LIB/DAL/DALPrescription.cs
+++ Hospital_MamSys_LIB/DAL/DALPrescription.cs
@@ -10,16 +10,105 @@ namespace Hospital_MamSys_LIB.DAL
 {
     internal class DALPrescription : DALBase
     {
-
+        // CREATE
         public void AddPrescription(Prescription p)
         {
             using (SqlConnection conn = new SqlConnection(connStr))
             using (SqlCommand cmd = new SqlCommand(
-                "INSERT INTO Prescription (RecordID, MedID, Quantity) VALUES (@r, @m, @q)", conn))
+                "INSERT INTO Prescription (RecordID, MedID, Quantity) " +
+                "VALUES (@r, @m, @q)", conn))
             {
                 cmd.Parameters.AddWithValue("@r", p.RecordID);
                 cmd.Parameters.AddWithValue("@m", p.MedID);
                 cmd.Parameters.AddWithValue("@q", p.Quantity);
+
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        // READ 
+        public Prescription GetPrescription(int recordId, int medId)
+        {
+            using (SqlConnection conn = new SqlConnection(connStr))
+            using (SqlCommand cmd = new SqlCommand(
+                "SELECT RecordID, MedID, Quantity " +
+                "FROM Prescription WHERE RecordID = @r AND MedID = @m", conn))
+            {
+                cmd.Parameters.AddWithValue("@r", recordId);
+                cmd.Parameters.AddWithValue("@m", medId);
+
+                conn.Open();
+                using (SqlDataReader rdr = cmd.ExecuteReader())
+                {
+                    if (rdr.Read())
+                    {
+                        return new Prescription
+                        {
+                            RecordID = rdr.GetInt32(0),
+                            MedID = rdr.GetInt32(1),
+                            Quantity = rdr.GetInt32(2)
+                        };
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        // READ 
+        public List<Prescription> GetAllPrescriptions()
+        {
+            List<Prescription> list = new List<Prescription>();
+
+            using (SqlConnection conn = new SqlConnection(connStr))
+            using (SqlCommand cmd = new SqlCommand(
+                "SELECT RecordID, MedID, Quantity FROM Prescription", conn))
+            {
+                conn.Open();
+                using (SqlDataReader rdr = cmd.ExecuteReader())
+                {
+                    while (rdr.Read())
+                    {
+                        list.Add(new Prescription
+                        {
+                            RecordID = rdr.GetInt32(0),
+                            MedID = rdr.GetInt32(1),
+                            Quantity = rdr.GetInt32(2)
+                        });
+                    }
+                }
+            }
+
+            return list;
+        }
+
+        // UPDATE
+        public void UpdatePrescription(Prescription p)
+        {
+            using (SqlConnection conn = new SqlConnection(connStr))
+            using (SqlCommand cmd = new SqlCommand(
+                "UPDATE Prescription SET Quantity = @q " +
+                "WHERE RecordID = @r AND MedID = @m", conn))
+            {
+                cmd.Parameters.AddWithValue("@q", p.Quantity);
+                cmd.Parameters.AddWithValue("@r", p.RecordID);
+                cmd.Parameters.AddWithValue("@m", p.MedID);
+
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        // DELETE 
+        public void DeletePrescription(int recordId, int medId)
+        {
+            using (SqlConnection conn = new SqlConnection(connStr))
+            using (SqlCommand cmd = new SqlCommand(
+                "DELETE FROM Prescription WHERE RecordID = @r AND MedID = @m", conn))
+            {
+                cmd.Parameters.AddWithValue("@r", recordId);
+                cmd.Parameters.AddWithValue("@m", medId);
 
                 conn.Open();
                 cmd.ExecuteNonQuery();
