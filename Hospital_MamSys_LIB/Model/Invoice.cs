4bfac9ce8518fe3573963ff5f2bcdf3eb82094ca
Zengpu Li <lizengpu@uwindsor.ca>
2025-11-08 19:32:02 -0500

    Add Invoice model and DAL implementation

--- Hospital_MamSys_LIB/Model/Invoice.cs
+++ Hospital_MamSys_LIB/Model/Invoice.cs
@@ -4,9 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Hospital_MamSys_LIB.Model
+namespace Hospital_ManSys_LIB.Model
 {
     internal class Invoice
     {
+        public int InvoiceID { get; set; }
+        public int PatientID { get; set; }
+        public int AppointmentID { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime DateIssued { get; set; }  
+        public string Status { get; set; } = "Unpaid";
     }
 }
